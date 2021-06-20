2468fa4e7e9ee893742184cf45f071a3a43a9d9f
alihanoz <alihanozmm@gmail.com>
2021-06-20 10:59:30 +0300

    Initial load

--- analyze.py
+++ analyze.py
@@ -5,15 +5,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 class analyze:
-	def __init__():
+    def __init__(self):
+        self.suggest_read_csv()
 
 
-	def suggest_read_csv(self, path='/kaggle/input'):
-	    for dir_name, _, file_names in os.walk('/kaggle/input'):
-	        for file_name in file_names:
-	            file = file_name.split('.')
-	            if file[-1] == 'csv':
-	                print(f"{file[0]} = pd.read_csv('{os.path.join(dir_name, file_name)}')")
+    def suggest_read_csv(self, path='/kaggle/input'):
+        for dir_name, _, file_names in os.walk('/kaggle/input'):
+            for file_name in file_names:
+                file = file_name.split('.')
+                if file[-1] == 'csv':
+                    print(f"{file[0]} = pd.read_csv('{os.path.join(dir_name, file_name)}')")
 
     def is_display_or_return(self, obj, is_display):
         if is_display:
@@ -42,88 +43,88 @@ class analyze:
         col_stats_df = reduce(lambda left,right: pd.merge(left,right,left_index=True,right_index=True,how='outer'), data_frames)
         return is_display_or_return(col_stats_df,display)
 
-	def define_df_cols(self, dataframe, cat_th=10, car_th=20, is_display=False):
+    def define_df_cols(self, dataframe, cat_th=10, car_th=20, is_display=False):
 
-	    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
-	    num_but_cat_cols = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
-	    cat_but_car_cols = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+        cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+        num_but_cat_cols = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+        cat_but_car_cols = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
 
-	    cat_cols = cat_cols + num_but_cat_cols
-	    cat_cols = [col for col in cat_cols if col not in cat_but_car_cols]
+        cat_cols = cat_cols + num_but_cat_cols
+        cat_cols = [col for col in cat_cols if col not in cat_but_car_cols]
 
-	    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
-	    num_cols = [col for col in num_cols if col not in num_but_cat_cols]
+        num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+        num_cols = [col for col in num_cols if col not in num_but_cat_cols]
 
-	    stats = pd.DataFrame(index=['Categoric Cols','Numeric Cols','Categoric But Car Cols','Numeric But Categoric Cols'])
-	    stats['Count'] = [len(cat_cols), len(num_cols), len(cat_but_car_cols), len(num_but_cat_cols)]
-	    stats['Columns'] = [", ".join(cat_cols), ", ".join(num_cols), ", ".join(cat_but_car_cols), ", ".join(num_but_cat_cols)]
-	    
-	    if is_display:
-	        display(stats)
-	    
-	    return cat_cols, num_cols, cat_but_car_cols, num_but_cat_cols
-
-
-	def plot_cols(dataframe, col_name, kind='bar'):
-	    plt.figure(figsize=(10,6))
-	    
-	    if kind == 'hist':
-	        ax = sns.histplot(dataframe[col_name], bins=20)
-	        plt.xlabel('')
-	    if kind == 'bar':
-	        dataframe.index.names = ['index']
-	        ax = sns.barplot(x='index', y=col_name, data=dataframe.reset_index())
-	        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-	        plt.xlabel('')
-
-	    plt.title(col_name)
-	    plt.tight_layout()
-	    plt.show()
+        stats = pd.DataFrame(index=['Categoric Cols','Numeric Cols','Categoric But Car Cols','Numeric But Categoric Cols'])
+        stats['Count'] = [len(cat_cols), len(num_cols), len(cat_but_car_cols), len(num_but_cat_cols)]
+        stats['Columns'] = [", ".join(cat_cols), ", ".join(num_cols), ", ".join(cat_but_car_cols), ", ".join(num_but_cat_cols)]
+        
+        if is_display:
+            display(stats)
+        
+        return cat_cols, num_cols, cat_but_car_cols, num_but_cat_cols
 
 
-	def cat_summary(dataframe, col_name, threshold=False, display=False, plot=False):
-	    df = pd.DataFrame({col_name: dataframe[col_name].value_counts(),"Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)})
-	    if threshold!=False:
-	        threshold_column = col_name if threshold > 1 else 'Ratio'
-	        if threshold <=1:
-	              df[threshold_column] = df[threshold_column]/100 
-	        main_df = df[df[threshold_column]>threshold]
-	        th_df = pd.DataFrame(df[df[threshold_column]<threshold].sum()).T
-	        th_df.index = ['Others']
-	        df = pd.concat([main_df,th_df])
-	    
-	    if plot:
-	        plot_cols(df,col_name)
-	        
-	    return is_display_or_return(df,display)
+    def plot_cols(self, dataframe, col_name, kind='bar'):
+        plt.figure(figsize=(10,6))
+        
+        if kind == 'hist':
+            ax = sns.histplot(dataframe[col_name], bins=20)
+            plt.xlabel('')
+        if kind == 'bar':
+            dataframe.index.names = ['index']
+            ax = sns.barplot(x='index', y=col_name, data=dataframe.reset_index())
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+            plt.xlabel('')
+
+        plt.title(col_name)
+        plt.tight_layout()
+        plt.show()
 
 
-	def num_summary(dataframe, col_name, display=False, plot=False,
-	               quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]):
+    def cat_summary(self, dataframe, col_name, threshold=False, display=False, plot=False):
+        df = pd.DataFrame({col_name: dataframe[col_name].value_counts(),"Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)})
+        if threshold!=False:
+            threshold_column = col_name if threshold > 1 else 'Ratio'
+            if threshold <=1:
+                  df[threshold_column] = df[threshold_column]/100 
+            main_df = df[df[threshold_column]>threshold]
+            th_df = pd.DataFrame(df[df[threshold_column]<threshold].sum()).T
+            th_df.index = ['Others']
+            df = pd.concat([main_df,th_df])
+        
+        if plot:
+            plot_cols(df,col_name)
+            
+        return is_display_or_return(df,display)
 
-	    df = pd.DataFrame(dataframe[col_name].describe(quantiles).T)
-	    
-	    if plot:
-	        plot_cols(dataframe,col_name,'hist')
-	        
-	    return is_display_or_return(df,display)
-	    
 
-	def col_target_summary(dataframe, target, col_name, display=False, plot=False):
-	    cat_cols, num_cols, cat_but_car_cols, num_but_cat_cols = define_df_cols(dataframe)
-	    
-	    if col_name in cat_cols+num_but_cat_cols:
-	        dataframe = pd.DataFrame({"TARGET_MEAN": dataframe.groupby(col_name)[target].mean()})
-	    elif col_name in num_cols+cat_but_car_cols:
-	        dataframe = dataframe.groupby(target).agg({col_name: "mean"}).sort_values(by=col_name,ascending=False)
-	    
-	    if plot:
-	        plot_cols(dataframe,'TARGET_MEAN')
-	        
-	    return is_display_or_return(dataframe, display)
-	    
+    def num_summary(self, dataframe, col_name, display=False, plot=False,
+                   quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]):
+
+        df = pd.DataFrame(dataframe[col_name].describe(quantiles).T)
+        
+        if plot:
+            plot_cols(dataframe,col_name,'hist')
+            
+        return is_display_or_return(df,display)
+        
+
+    def col_target_summary(self, dataframe, target, col_name, display=False, plot=False):
+        cat_cols, num_cols, cat_but_car_cols, num_but_cat_cols = define_df_cols(dataframe)
+        
+        if col_name in cat_cols+num_but_cat_cols:
+            dataframe = pd.DataFrame({"TARGET_MEAN": dataframe.groupby(col_name)[target].mean()})
+        elif col_name in num_cols+cat_but_car_cols:
+            dataframe = dataframe.groupby(target).agg({col_name: "mean"}).sort_values(by=col_name,ascending=False)
+        
+        if plot:
+            plot_cols(dataframe,'TARGET_MEAN')
+            
+        return is_display_or_return(dataframe, display)
+        
                 
-suggest_read_csv()
+analyzer = analyze()
 
 sample_submission = pd.read_csv('/kaggle/input/house-prices-advanced-regression-techniques/sample_submission.csv')
 train = pd.read_csv('/kaggle/input/house-prices-advanced-regression-techniques/train.csv')
